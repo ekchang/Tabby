d242e51a78af76537e33345111a2b2d5ca800347
Erick Chang <ekc.dev@gmail.com>
2015-09-30 13:24:04 -0400

    Correct PendingIntent flag

--- app/src/main/java/com/hitherejoe/tabby/ui/activity/MainActivity.java
+++ app/src/main/java/com/hitherejoe/tabby/ui/activity/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onStop() {
+        Timber.d("onStop");
         super.onStop();
         mCustomTabActivityHelper.unbindCustomTabsService(this);
     }
@@ -161,7 +162,8 @@ public class MainActivity extends BaseActivity {
                 .subscribeOn(Schedulers.io())
                 .subscribe(new Subscriber<Bitmap>() {
                     @Override
-                    public void onCompleted() { }
+                    public void onCompleted() {
+                    }
 
                     @Override
                     public void onError(Throwable e) {
@@ -184,14 +186,14 @@ public class MainActivity extends BaseActivity {
                 Intent.ACTION_SENDTO, Uri.fromParts("mailto", "example@example.com", null));
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
         emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
-        return PendingIntent.getActivity(getApplicationContext(), 0, emailIntent, 0);
+        return PendingIntent.getActivity(getApplicationContext(), 0, emailIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     private PendingIntent createPendingShareIntent() {
         Intent actionIntent = new Intent(Intent.ACTION_SEND);
         actionIntent.setType("text/plain");
         actionIntent.putExtra(Intent.EXTRA_TEXT, "This is sharing some text");
-        return PendingIntent.getActivity(getApplicationContext(), 0, actionIntent, 0);
+        return PendingIntent.getActivity(getApplicationContext(), 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     // You can use this callback to make UI changes
